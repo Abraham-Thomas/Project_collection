fafe1e14b8b472cc68f7dec7d75ace0345f6c051
Abraham-Thomas <15197498389@163.com>
2019-02-27 23:34:39 +0800

    category mddule built

--- MiniProjects/First/pages/query/category/category.js
+++ MiniProjects/First/pages/query/category/category.js
@@ -1,4 +1,48 @@
+const app = getApp();
 Page({
-  data: {},
-  onLoad() {},
+	data: {
+		catList: []
+	},
+	//页面加载
+	onLoad() {},
+
+	searchItems(e) {
+		// console.log(e);
+		//获取搜索框中的值
+		var itemName = e.detail.value
+		if(itemName != null && itemName != '' && itemName != undefined) {
+			my.navigateTo({
+				url: "/pages/query/list/list?searchType=words&itemName=" + itemName
+			});
+		}
+		// else {
+		// 	return;
+		// }
+	},
+
+	onReady() {
+		var me = this;
+		//请求分类数据
+		// my.httpRequest({
+		// 	url: app.serverUrl + '/cats', // 目标服务器url
+		// 	method: 'POST',
+		// 	header: {
+		// 		'content-type': 'application/json'
+		// 	},
+		// 	dataType: 'json',
+		// 	success: (res) => {
+		// 		console.log(res);
+		// 		//获取拿到后端的数据
+		// 		var myData = res.data;
+		// 		if(myData.status == 200) {
+		// 			var catList = myData.data;
+		// 			//把新的数据重新覆盖数据绑定中原有的值
+		// 			me.setData({
+		// 				catList: catList
+		// 			});
+		// 		}
+		// 	}
+		// });
+	}
+
 });
---
+++ MiniProjects/First/pages/query/list/list.js
@@ -0,0 +1,9 @@
+Page({
+  data: {},
+  onLoad(params) {
+	  var searchType = params.searchType;
+	  var itemName = params.itemName;
+	  console.log("searchType:" + searchType);
+	  console.log("itemName:" + itemName);
+  },
+});
