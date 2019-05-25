1d44c7e7dcb594caee5490c66d78c62613d962c8
Abraham-Thomas <15197498389@163.com>
2019-05-25 18:49:01 +0800

    new vuepage

---
+++ vue-simplepage/src/pages/home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <home-header />
+    <home-swiper />
+    <home-icons />
+  </div>
+</template>
+
+<script>
+  // @ is an alias to /src
+  import HomeHeader from './components/Header'
+  import HomeSwiper from './components/Swiper'
+  import HomeIcons from './components/Icons'
+  export default {
+    name: 'home',
+    components: {
+      HomeIcons,
+      HomeHeader,
+      HomeSwiper
+    }
+  }
+</script>
