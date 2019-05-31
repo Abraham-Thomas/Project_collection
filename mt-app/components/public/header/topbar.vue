100d425c39b016575b56161094ac0c70c46a1c10
Abraham-Thomas <15197498389@163.com>
2019-05-31 23:29:13 +0800

    register

---
+++ mt-app/components/public/header/topbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-row
+    :gutter="0"
+    class="m-header">
+    <el-col :span="5">
+      <geo/>
+    </el-col>
+    <el-col :span="5">
+      <user/>
+    </el-col>
+    <el-col :span="14">
+      <navbar/>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+  import Geo from './geo.vue'
+  import User from './user.vue'
+  import Navbar from './nav.vue'
+
+  export default {
+    name: 'topBar',
+    components: {
+      Geo,
+      User,
+      Navbar
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "../../../assets/css/public/header/index";
+  @import "../../../assets/css/public/layout";
+</style>
