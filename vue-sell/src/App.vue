c517eecd7da66bda8308d53057a7f84f10926535
Abraham-Thomas <15197498389@163.com>
2019-06-11 09:53:57 +0800

    prepare project

--- vue-sell/src/App.vue
+++ vue-sell/src/App.vue
@@ -1,51 +1,26 @@
 <template>
   <div id="app">
-    <v-header :seller="seller"/>
-    <div class="tab-wrapper">
-      <tab :tabs="tabs"></tab>
-    </div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-  import { getSeller } from 'api'
-  import VHeader from './components/v-header/v-header'
-  import Goods from './components/goods/goods'
-  import Ratings from './components/ratings/ratings'
-  import Seller from './components/seller/seller'
-  import Tab from './components/tab/tab'
+import HelloWorld from './components/HelloWorld.vue'
 
-  export default {
-    name: 'app',
-    data () {
-      return {
-        seller: {}
-      }
-    },
-    computed: {
-      tabs () {
-        return [
-          { label: '商品', component: Goods, data: { seller: this.seller } },
-          { label: '评价', component: Ratings, data: { seller: this.seller } },
-          { label: '商家', component: Seller, data: { seller: this.seller } }
-        ]
-      }
-    },
-    created () {
-      this._getSeller()
-    },
-    methods: {
-      _getSeller () {
-        getSeller().then((seller) => {
-          this.seller = seller
-        })
-      }
-    },
-    components: {
-      Tab,
-      VHeader
-    }
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
   }
+}
 </script>
 <style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
 </style>
--- vue-sell/vue.config.js
+++ vue-sell/vue.config.js
@@ -1,12 +1,7 @@
-const path = require('path')
-const appData = require('./data')
+const appData = require('./data.json')
 const seller = appData.seller
 const goods = appData.goods
 const ratings = appData.ratings
-
-function resolve(dir) {
-  return path.join(__dirname, dir)
-}
 
 module.exports = {
   css: {
@@ -26,31 +21,25 @@ module.exports = {
     }
   },
   devServer: {
-    before(app) {
-      app.get('/api/seller',function (req,res) {
+    before (app) {
+      app.get('/api/seller', function (req, res) {
         res.json({
           errno: 0,
           data: seller
         })
       })
-      app.get('/api/goods',function (req,res) {
+      app.get('/api/goods', function (req, res) {
         res.json({
           errno: 0,
           data: goods
         })
       })
-      app.get('/api/ratings',function (req,res) {
+      app.get('/api/ratings', function (req, res) {
         res.json({
           errno: 0,
           data: ratings
         })
       })
     }
-  },
-  chainWebpack(config) {
-    config.resolve.alias
-      .set('components', resolve('src/components'))
-      .set('common', resolve('src/common'))
-      .set('api', resolve('src/api'))
   }
 }
